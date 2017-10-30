82f6f36adceb647913318a7fe592eb363254b0e9
yesharge <yeshargeabrha@gmail.com>
2017-10-30 08:07:04 -0400

    3rd commit

--- src/main/java/com/reviewssite/reviewssite/ReviewRepository.java
+++ src/main/java/com/reviewssite/reviewssite/ReviewRepository.java
@@ -6,28 +6,29 @@ import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class ReviewRepository {
 
 	protected Map<Long, Reviews> reviews = new HashMap<Long, Reviews>();
 
 	public ReviewRepository() {
-		Reviews review1 = new Reviews(111L, "Never Again", "/images/jamesbond.jpg", "Anastasia",
-				"Never again will I watch another James Bond movie");
+		Reviews review1 = new Reviews(111L, "Brow Dip", "/images/anastasia.jpg", "Anastasia",
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum itaque ex a dignissimos necessitatibus. Reprehenderit nobis quae hic tempora voluptas, dolorum quidem expedita error, non cum quia accusamus recusandae rerum");
 		Reviews review2 = new Reviews(222L, "Trophy Wife", "/images/trophywife.jpg", "Fenty Beauty",
-				"I looveeee Rihanna's new highlight. Can see my highlight from space. Will buy again. ");
-		Reviews review3 = new Reviews(333L, "Trophy Wife", "/images/trophywife.jpg", "Fenty Beauty",
-				"I looveeee Rihanna's new highlight. Can see my highlight from space. Will buy again. ");
-	reviews.put(review1.getId(), review1);
-	reviews.put(review2.getId(), review2);
-	reviews.put(review3.getId(), review3);
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum itaque ex a dignissimos necessitatibus. Reprehenderit nobis quae hic tempora voluptas, dolorum quidem expedita error, non cum quia accusamus recusandae rerum");
+		Reviews review3 = new Reviews(333L, "Champagne Pop", "/images/becca.jpg", "Becca",
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum itaque ex a dignissimos necessitatibus. Reprehenderit nobis quae hic tempora voluptas, dolorum quidem expedita error, non cum quia accusamus recusandae rerum");
+		reviews.put(review1.getId(), review1);
+		reviews.put(review2.getId(), review2);
+		reviews.put(review3.getId(), review3);
 	}
+
 	public Collection<Reviews> findAll() {
 		return reviews.values();
-		
+
 	}
-	public Reviews findOne (Long id) {
+
+	public Reviews findOne(Long id) {
 		return reviews.get(id);
 	}
 }
--- src/main/java/com/reviewssite/reviewssite/ReviewsController.java
+++ src/main/java/com/reviewssite/reviewssite/ReviewsController.java
@@ -11,15 +11,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ReviewsController {
 	@Resource
 	ReviewRepository repository = new ReviewRepository();
+
 	@RequestMapping("/reviews")
-    public String showAllReviews(Model model) {
-        model.addAttribute("reviews", repository.findAll()); 
-        return "reviews"; 
-    }
+	public String showAllReviews(Model model) {
+		model.addAttribute("reviews", repository.findAll());
+		return "reviews";
+	}
+
 	@RequestMapping("/review")
-    public String showOneReview(@RequestParam(value = "id") Long id, Model model) {
-        model.addAttribute("reviews", repository.findOne(id)); 
-        return "review"; 
-    }
+	public String showOneReview(@RequestParam(value = "id") Long id, Model model) {
+		model.addAttribute("reviews", repository.findOne(id));
+		return "review";
+	}
 
 }
